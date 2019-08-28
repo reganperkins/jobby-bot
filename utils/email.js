9e514df77e2d5c2adda1eab12e6625c7121b045d
reganperkins <reganlperkins@gmail.com>
2019-08-28 15:19:27 -0700

    use mailgun api not stmp

--- utils/email.js
+++ utils/email.js
@@ -1,43 +1,29 @@
-
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-  host: 'smtp.mailgun.org',
-  port: 587,
-  auth: {
-    type: 'login',
-    user: process.env.emailUsername,
-    pass: process.env.emailPassword,
-  },
-});
+const mailgun = require('mailgun-js')({ apiKey: process.env.apiKey, domain: process.env.domain });
 
 const createMailOptions = content => ({
-  from: `Recruiter Bot <${process.env.email}>`,
+  from: `Recruiter Bot <${ process.env.email }>`,
   to: process.env.emailRecipient,
   subject: 'Email from recruiter bot',
-  html: content,
+  html: content
 });
 
 const createTableStringFromObject = (contentObject) => {
   const keyArray = Object.keys(contentObject);
-  const tableRows = keyArray.map((key) => `<tr style="text-align: left;"><th style="width: 100px; padding: 10px 0;">${key}:</th><td style="padding: 10px 0;">${contentObject[key]}</td></tr>`);
+  const tableRows = keyArray.map((key) => `<tr style="text-align: left;"><th style="width: 100px; padding: 10px 0;">${ key }:</th><td style="padding: 10px 0;">${ contentObject[key] }</td></tr>`);
   return (
-    `<table style="width: 100%;"><tbody>${tableRows.join('')}</tbody></table>`
+    `<table style="width: 100%;"><tbody>${ tableRows.join('') }</tbody></table>`
   );
-}
-
+};
 
 const sendMail = (mailOptions) => {
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.log('ERROR:', err);
-    } 
-    transporter.close();
+  mailgun.messages().send(mailOptions, (error, body) => {
+    if (error) console.log(error);
+    console.log(body);
   });
 };
 
 module.exports = {
   sendMail,
   createMailOptions,
-  createTableStringFromObject,
+  createTableStringFromObject
 };
